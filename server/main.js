57000628406756a43a5d813ec6e291abcab9c4d8
Jeonilju <lanace93@gmail.com>
2016-08-07 03:55:26 +0900

    세션 모듈 및 바디파서 추가

--- server/main.js
+++ server/main.js
@@ -1,18 +1,27 @@
+'use strict';
+
+import apis from './routes/apis';
+import bodyParser from 'body-parser';
 import express from 'express';
- 
+import session from 'express-session';
+
 const app = express();
- 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use('/', express.static(__dirname + '/../dist'));
  
-app.get('/hello', (req, res) => {
-    return res.send('Can you hear me?');
-});
- 
-import apis from './routes/apis';
-import users from './routes/users';
+app.use(session({
+  secret: 'linkto', // 비밀키
+  saveUninitialized: true,
+  resave: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 // 쿠키 유효기간 1시간
+  }
+}));
 
 app.use('/apis', apis);
-app.use('/users', users);
 
 const server = app.listen(3001, () => {
     console.log('Express listening on port 3001');
