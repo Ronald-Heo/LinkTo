67ddf6f8094850f30366144f12ea080d29751131
Jeonilju <lanace93@gmail.com>
2016-08-18 18:58:26 +0900

    nvd3 제거 및 안쓰는 코드 제거

--- server/routes/controllers/tables.js
+++ server/routes/controllers/tables.js
@@ -31,17 +31,8 @@ router.get('/controller', (req, res) => {
             res.send(_.groupBy(result, 'ItemTimeStamp'));
         });
 	} else {
-        controllers.getControllerData('fic001', function(err, result) {
-            // 필요한 데이터로 걸러냄 
-            result = _.map(result, (category) => {
-                category.ItemID = category.ItemID.split('.')[1].replace('\n"', '').replace('\"', ''); // TODO 잘못된 데이터가 듫어가 있어서 임의로 삽입한 코드
-                return category;
-            });
-            
-            // res.send(_.groupBy(result, 'ItemID'));
-            console.log(result);
-            res.send(result);
-        });
+        res.status(400);
+        res.send('no selected table');
     }
 });
 
